22cae017f79110504591903d36071feece48326f
SLuvizotto <luvizottodev@gmail.com>
2025-01-14 01:13:17 -0300

    Atualização de routes

--- lib/controllers/app_routes.dart
+++ lib/controllers/app_routes.dart
@@ -1,15 +1,16 @@
 import 'package:controle_vendas_e_estoque/view/auth/login_screen.dart';
 import 'package:controle_vendas_e_estoque/view/auth/register_screen.dart';
 import 'package:controle_vendas_e_estoque/view/home_screen.dart';
+import 'package:controle_vendas_e_estoque/view/product_creation_screen.dart';
 import 'package:flutter/material.dart';
-
 
 class AppRoutes {
   static const login = '/login';
   static const register = '/register';
   static const home = '/home';
+  static const productCreation = '/product-creation';
 
-  static Route<dynamic> generateRoute(RouteSettings settings) {
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case login:
         return MaterialPageRoute(builder: (_) => LoginScreen());
@@ -17,12 +18,10 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => RegisterScreen());
       case home:
         return MaterialPageRoute(builder: (_) => HomeScreen());
-        default:
-        return MaterialPageRoute(
-          builder: (_) => const Scaffold(
-            body: Center(child: Text("Rota não encontrada")),
-          ),
-        );
+      case productCreation:
+        return MaterialPageRoute(builder: (_) => const ProductCreationScreen());
+      default:
+        return null;
     }
   }
 }
