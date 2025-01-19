27516f92f0507720b7b554a9da941d54715cf662
SLuvizotto <luvizottodev@gmail.com>
2025-01-19 00:58:23 -0300

    Atualização de cor

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryBackground = Colors.black; // Fundo principal.
-  static const Color primaryAccent = Color(0xFFD4AF37); // Cor primária para destaques.
-  static const Color primaryText = Colors.white; // Dourado.
+  static const Color primaryBackground = Colors.black; // Fundo principal
+  static const Color primaryAccent = Color(0xFFDD731F); // Destaque principal
+  static const Color primaryText = Colors.white; // Cor do texto
 }
