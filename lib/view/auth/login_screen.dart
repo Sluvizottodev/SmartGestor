6772cee83a70ceafcd665040fceca7c5638a9d19
SLuvizotto <luvizottodev@gmail.com>
2025-01-19 00:59:04 -0300

    Modernização das telas login e registro

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -1,96 +1,95 @@
-import 'package:controle_vendas_e_estoque/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:controle_vendas_e_estoque/controllers/auth_controller.dart';
+
+import '../../utils/app_colors.dart';
 
 class LoginScreen extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final AuthController _authController = AuthController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [AppColors.primaryBackground, AppColors.primaryAccent],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
+        decoration: const BoxDecoration(color: AppColors.primaryBackground),
         child: Center(
           child: SingleChildScrollView(
             padding: const EdgeInsets.all(16.0),
-            child: Card(
-              color: Colors.white.withOpacity(0.9),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.primaryAccent,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                        filled: true,
-                        fillColor: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Senha",
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                        filled: true,
-                        fillColor: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 24),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                        backgroundColor: AppColors.primaryAccent,
-                      ),
-                      onPressed: () {
-                        _authController.signIn(
-                          context,
-                          _emailController.text.trim(),
-                          _passwordController.text.trim(),
-                        );
-                      },
-                      child: Center(
-                        child: Text(
-                          "Entrar",
-                          style: TextStyle(fontSize: 18, color: AppColors.primaryText),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextButton(
-                      onPressed: () => Navigator.pushNamed(context, '/register'),
-                      child: const Text(
-                        "Cadastrar-se",
-                        style: TextStyle(fontSize: 16, color: AppColors.primaryAccent),
-                      ),
-                    ),
-                  ],
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logoSF-W.png',
+                  height: 250,
                 ),
-              ),
+                const SizedBox(height: 32),
+                TextField(
+                  controller: _emailController,
+                  style: const TextStyle(color: AppColors.primaryText),
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: const TextStyle(color: AppColors.primaryText),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryText),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryAccent),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  style: const TextStyle(color: AppColors.primaryText),
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    labelStyle: const TextStyle(color: AppColors.primaryText),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryText),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryAccent),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primaryAccent,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Center(
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: AppColors.primaryText,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextButton(
+                  onPressed: () => Navigator.pushNamed(context, '/register'),
+                  child: const Text(
+                    "Cadastrar-se",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: AppColors.primaryAccent,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
--- lib/view/auth/register_screen.dart
+++ lib/view/auth/register_screen.dart
@@ -1,6 +1,10 @@
 import 'package:controle_vendas_e_estoque/utils/app_colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:controle_vendas_e_estoque/controllers/auth_controller.dart';
+
+import '../../models/company_model.dart';
+import '../../services/firebase_service.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -14,126 +18,145 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _cnpjController = TextEditingController();
-  final AuthController _authController = AuthController();
-  bool _isCnpjRequired = false; // Variável para controlar a caixa de seleção
+  bool _showCnpjField = false; // Controle para mostrar/ocultar o campo CNPJ
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [AppColors.primaryBackground, AppColors.primaryAccent],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
+        decoration: const BoxDecoration(color: AppColors.primaryBackground),
         child: Center(
           child: SingleChildScrollView(
             padding: const EdgeInsets.all(16.0),
-            child: Card(
-              color: Colors.white.withOpacity(0.9),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logoSF-W.png',
+                  height: 250,
+                ),
+                const SizedBox(height: 32),
+                TextField(
+                  controller: _nameController,
+                  style: const TextStyle(color: AppColors.primaryText),
+                  decoration: InputDecoration(
+                    labelText: "Nome",
+                    labelStyle: const TextStyle(color: AppColors.primaryText),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryText),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryAccent),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _emailController,
+                  style: const TextStyle(color: AppColors.primaryText),
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: const TextStyle(color: AppColors.primaryText),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryText),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryAccent),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  style: const TextStyle(color: AppColors.primaryText),
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    labelStyle: const TextStyle(color: AppColors.primaryText),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryText),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: AppColors.primaryAccent),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Text(
-                      "Cadastro",
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.primaryAccent,
-                      ),
+                      "Informar CNPJ",
+                      style: TextStyle(color: AppColors.primaryText),
                     ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _nameController,
-                      style: TextStyle(color: Colors.black), // Define a cor do texto como preto
-                      decoration: InputDecoration(
-                        labelText: "Nome",
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                        filled: true,
-                        fillColor: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _emailController,
-                      style: TextStyle(color: Colors.black), // Define a cor do texto como preto
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                        filled: true,
-                        fillColor: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      style: TextStyle(color: Colors.black), // Define a cor do texto como preto
-                      decoration: InputDecoration(
-                        labelText: "Senha",
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                        filled: true,
-                        fillColor: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    CheckboxListTile(
-                      title: const Text(
-                        "Deseja informar o CNPJ?",
-                        style: TextStyle(color: AppColors.primaryAccent), // Cor alterada
-                      ),
-                      value: _isCnpjRequired,
-                      onChanged: (bool? value) {
+                    Switch(
+                      value: _showCnpjField,
+                      onChanged: (bool value) {
                         setState(() {
-                          _isCnpjRequired = value ?? false;
+                          _showCnpjField = value;
                         });
                       },
                     ),
-                    if (_isCnpjRequired) // Só exibe se o checkbox estiver marcado
-                      const SizedBox(height: 16),
-                    if (_isCnpjRequired)
-                      TextField(
-                        controller: _cnpjController,
-                        style: TextStyle(color: Colors.black), // Define a cor do texto como preto
-                        decoration: InputDecoration(
-                          labelText: "CNPJ",
-                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                          filled: true,
-                          fillColor: Colors.white,
-                        ),
-                      ),
-                    const SizedBox(height: 24),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                        backgroundColor: AppColors.primaryAccent,
-                      ),
-                      onPressed: () {
-                        _authController.registerUser(
-                          context,
-                          _nameController.text.trim(),
-                          _emailController.text.trim(),
-                          _passwordController.text.trim(),
-                          _isCnpjRequired ? _cnpjController.text.trim() : null, // CNPJ será null se não for informado
-                        );
-                      },
-                      child: Center(
-                        child: Text(
-                          "Cadastrar",
-                          style: TextStyle(fontSize: 18, color: AppColors.primaryText),
-                        ),
-                      ),
-                    ),
                   ],
                 ),
-              ),
+                if (_showCnpjField)
+                  TextField(
+                    controller: _cnpjController,
+                    style: const TextStyle(color: AppColors.primaryText),
+                    decoration: InputDecoration(
+                      labelText: "CNPJ",
+                      labelStyle: const TextStyle(color: AppColors.primaryText),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(color: AppColors.primaryText),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(color: AppColors.primaryAccent),
+                      ),
+                    ),
+                  ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primaryAccent,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: () async {
+                    String email = _emailController.text;
+                    String password = _passwordController.text;
+                    String name = _nameController.text;
+                    String? cnpj = _showCnpjField ? _cnpjController.text : null;
+
+                    await FirebaseService().registerCompany(email, password);
+                    if (cnpj != null && cnpj.isNotEmpty) {
+                      await FirebaseService().saveCompanyToFirestore(
+                        CompanyModel(name: name, email: email, cnpj: cnpj),
+                        FirebaseAuth.instance.currentUser!.uid,
+                      );
+                    }
+                  },
+                  child: const Center(
+                    child: Text(
+                      "Cadastrar",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: AppColors.primaryText,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
